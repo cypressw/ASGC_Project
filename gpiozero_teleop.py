43c308f36874b90437aa19f1fbecd97947cddca6
linzhangUCA <lzhang12@uca.edu>
2021-08-26 17:34:45 -0500

    upgrade gpiozero_teleop with PhaseEnableRobot

--- gpiozero_teleop.py
+++ gpiozero_teleop.py
@@ -1,14 +1,18 @@
 import curses
-from gpiozero import Robot
+from gpiozero import LED, PhaseEnableRobot
 
-robot = Robot(left=(12, 24, 22), right=(13, 25, 23))
+# Specify motor enable GPIO pins, MC33926 is a PHASE/ENABLE motor driver.
+en_motor1 = LED(22) # Make GPIO mode as output using LED class
+en_motor2 = LED(23) # In case you wan to set GPIO mode as input, go with Button class
+
+# robot = Robot(left=(12, 24, 22), right=(13, 25, 23))
+robot = PhaseEnableRobot(left=(24, 12), right=(25, 13)) # (Motor PWM, Motor DIR)
 
 actions = {
     curses.KEY_UP:    robot.forward,
     curses.KEY_DOWN:  robot.backward,
     curses.KEY_LEFT:  robot.left,
     curses.KEY_RIGHT: robot.right,
-    curses.ERR:       robot.stop,
 }
 
 
@@ -16,6 +20,10 @@ screen = curses.initscr()
 curses.noecho()
 curses.halfdelay(1)       
 screen.keypad(True)
+
+# Enable motors
+en_motor1.on()
+en_motor2.on()
 
 try:
 
@@ -30,13 +38,19 @@ try:
         
         # call appropriate robot function
         else:
-            action = actions.get(char)
-            action()
+            if not char in actions:
+                robot.stop()
+            else:
+                action = actions.get(char)
+                if action is not None:
+                    action(.6) # to safely use a 7.4V battery on a 6V nominal motor, full duty cycle is not recommended
 
 
 finally:
     # Stop the motors, even if there is an exception
     # or the user presses Ctrl+C to kill the process.
+    en_motor1.off()
+    en_motor2.off()
     robot.stop()
 
     # Close down curses properly and turn echo back on
